51681e6f4462c668211bfc2f9db9a46c3a83e17b
Luke Prescott <lprescott@albany.edu>
2018-10-10 15:38:29 -0400

    Initial Commit

---
+++ assembler.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *ltrim(char *s) {
+	while (*s == ' ' || *s == '\t') s++;
+	return s;
+}
+
+char getRegister(char *text) {
+	if (*text == 'r' || *text=='R') text++;
+	return atoi(text);
+}
+
+int assembleLine(char *text, unsigned char* bytes) {
+	text = ltrim(text);
+	char *keyWord = strtok(text," ");
+	if (strcmp("add",keyWord) == 0) {
+		bytes[0] = 0x10;
+		bytes[0] |= getRegister(strtok(NULL," "));
+		bytes[1] = getRegister(strtok(NULL," ")) << 4 | getRegister(strtok(NULL," "));
+		return 2;
+	}
+}
+
+int main(int argc, char **argv) {
+	FILE *src = fopen(argv[1],"r");
+	FILE *dst = fopen(argv[2],"w");
+	while (!feof(src)) {
+		unsigned char bytes[4];
+		char line[1000];
+		printf ("about to read\n");
+		if (NULL != fgets(line, 1000, src)) {
+			printf ("read: %s\n",line);
+			int byteCount = assembleLine(line,bytes);
+			fwrite(bytes,byteCount,1,dst);
+		}
+	}
+	fclose(src);
+	fclose(dst);
+	return 0;
+}
